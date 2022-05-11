12b172d4c6e33c721fb28bf14ebe1351e5d7f21e
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-05-11 22:48:02 +0530

    commit

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,41 +2,44 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className='text-gray-800'>
+    <div className='text-gray-800 flex'>
 
-        <div>
-            <img src="" alt="" />
+        <div className='flex sm:w-1/2 sm:max-h-screen'>
+            <style>
+                <img src="" alt="" />
+            </style>
         </div>
 
-        <div className='p-10'>
+        <div className='text-teal-800 p-8 font-semibold w-1/2'>
 
-            <div className='font-bold text-2xl p-6'>
-                Login
+            <div className='font-extrabold text-2xl p-6 underline'>
+                Sign Up
             </div>
 
-            <div className='p-2'>
+            <div className='p-3 mb-2 rounded-full shadow-lg hover:bg-white'>
                 First Name :
                 <input type="text" placeholder=' Type here' name='firstName'/>
             </div>
 
-            <div className='p-2'>
+            <div className='p-3 mb-2 rounded-full shadow-lg hover:bg-white'>
                 Last Name :
                 <input type="text" placeholder=' Type here' name='lastName'/>
             </div>
 
-            <div className='p-2'>
+            <div className='p-3 mb-2 rounded-full shadow-lg hover:bg-white'>
                 E-mail Address : 
                 <input type="text" placeholder=" Type here" name='email'/>
             </div>
 
-            <div className='p-2'>
+            <div className='p-3 mb-4 rounded-full shadow-lg hover:bg-white'>
                 Password : 
                 <input type="password" placeholder=" Type here" name='pass'/>
             </div>
 
-            <div className='bg-teal-600 shadow-sm rounded-full font-bold text-center'>
-                <button>Login</button>
+            <div className='bg-teal-600 shadow-md rounded-full text-center text-white p-2 mb-2 hover:bg-teal-700' >
+                <button>Sign Up</button>
             </div>
+           
 
         </div>
 
